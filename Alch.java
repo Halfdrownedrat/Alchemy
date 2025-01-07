5040ea614d0e79a91865967949bb907ff360f3fe
Halfdrownedrat <terranauticarocket@gmail.com>
2025-01-07 14:36:47 +0100

    Cauldren Cap and Aspect Values for states - Cauldren now has a max cap that blocks any more ingredients from being added (Currently uses aspect as the number that caps) - Aspect Values are not set for each state (5 for each state, all saved in an int[25])

--- Alch.java
+++ Alch.java
@@ -13,8 +13,9 @@ public class Alch{
 
     // Aggregatszustände 
     // States Nrm, Slc, Pwd, dis, des, Cri == Normal, Sliced, Powddered, Dissolved, Destilled, Cristalline
-    final static String[] states = {" Nrm", "Slc ", "Pwd ", "Dis ", "Des ", "Cri "}; // 
+    final static String[] states = {"Nrm", "Slc ", "Pwd ", "Dis ", "Des ", "Cri "}; // 
     int[] aspectValues;
+    int capacity;
 
 
     public static void main(String[] args) {
@@ -24,8 +25,8 @@ public class Alch{
     }
     // Called before  the game actually does stuff  
     public static void startUp(){
-        FileToHash("ingredients.csv", ingredients);
-        FileToHash("aspects.csv", aspects);
+        FileToHash("ingredients.csv", ingredients, 5);
+        FileToHash("aspects.csv", aspects, 25);
     }
     
     // Main Menu that gets called everytime the type of action changes
@@ -70,7 +71,7 @@ public class Alch{
 
     // Read data from file and store it in hasmap for easier working
     // csv needs to be whitespace free
-    public static void FileToHash(String file, Map<String, int[]> map){
+    public static void FileToHash(String file, Map<String, int[]> map, int valueAmount){
         File ing = new File(file);
         try (Scanner fileScanner = new Scanner(ing)) {
             fileScanner.nextLine(); // Skip the first line since it has comments in it
@@ -78,7 +79,7 @@ public class Alch{
                 String line = fileScanner.nextLine();
                 String[] parts = line.split(",");
                 String ingredient = (String) parts[0];
-                int[] counts = new int[5];
+                int[] counts = new int[valueAmount];
                 for (int i = 1; i < parts.length; i++) {
                     counts[i - 1] = Integer.parseInt(parts[i]);
                 }
@@ -94,7 +95,6 @@ public class Alch{
     }
 
     // Print out ALL the info 
-    // Needs some compacter formatting later, a table would be smart
     // Fixed with Copilot
     public static void OUT_ALL() {
         clearScreen();
@@ -123,7 +123,7 @@ public class Alch{
             for (int count : counts) {
                 System.out.printf("%-" + stateColumnWidth + "s", "x" + count);
             }
-            System.out.println(); // New line
+            System.out.println(); 
         }
     }
     
@@ -194,7 +194,7 @@ public class Alch{
         System.out.println("--------------------------------------------------------------------");
         System.out.println("Enter the ingredient and the amount to sacrefice to the pot");
         String putIN = " ";
-        Alch mainPot = new Alch(999);// Standart Debugging Pot and the only one in the current version
+        Alch mainPot = new Alch(50);// Standart Pot and the only one in the current version
         while (true) {
             getPotData(mainPot);
             if (putIN.equals("exit")) {
@@ -207,6 +207,7 @@ public class Alch{
             }
             //
             // Need some better way to call the correct Ingredient
+            // Currently only uses the starting resoure
             //
             ingredients.get(putIN)[0]--;
             mainPot.changeIngredient(putIN, 1);
@@ -219,10 +220,19 @@ public class Alch{
         System.out.println("You have those things in your pot:");
         System.out.println("Air, Fire, Earth, Water, Order, Entropy, Chaos, Flux\n" + Arrays.toString(currentPot.aspectValues));
     }
+    public static boolean hasSpace(Alch currentPot){
+        int occupied = 0;
+        for (int i = 0; i < currentPot.aspectValues.length; i++) {
+            occupied += currentPot.aspectValues[i];
+        }
+        return occupied < currentPot.capacity;
+
+    }
 
     // Creates an Object of the brewing pot, maily doing it this way to train for university
     public Alch(int Capacity){
         //Air, Fire, Earth, Water, Order, Entropy, Chaos, Flux
+        capacity = Capacity;
         aspectValues = new int[5];
     }
 
@@ -241,14 +251,19 @@ public class Alch{
     public void changeIngredient(String material, int action){
         ingredients.get(material)[0]--;
         for (int i = 0; i < this.aspectValues.length; i++) {
+            if (!hasSpace(this)) {
+                System.out.println("Cauldren is full");
+                break;
+            }
             this.aspectValues[i]+= action * aspects.get(material)[i];
+
         }
     }
 
     // The Text Block Stuff is fun and usefull but blocks to much space
     private static String GetTextblock(){
         return """
-                Enter Up to 5 different ingredients to the pot.
+                Enter Up to 999 different ingredients to the pot.
                 Leftovers can only be voidet at the moment, unlock newer tech/ wait for a newer game version to change that.
                 Enter the Name and Amount to add them to the pot. Type "Brew" to finish the mixture.
                 """;
